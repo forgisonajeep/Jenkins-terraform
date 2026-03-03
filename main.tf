cb68a6b21938df16912b8fcae288f1860716ac69
forgisonajeep <parker_c_18@yahoo.com>
2026-03-02 19:11:37 -0500

    Complex: implement IAM role-based S3 access for EC2

--- main.tf
+++ main.tf
@@ -75,6 +75,7 @@ resource "aws_instance" "jenkins" {
   instance_type          = var.instance_type
   subnet_id              = data.aws_subnets.default_vpc_subnets.ids[0]
   vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.jenkins_instance_profile.name
 
 
   key_name = var.key_pair_name
@@ -104,4 +105,61 @@ resource "aws_instance" "jenkins" {
 
 output "jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
+}
+
+# IAM Policy for S3 Access
+resource "aws_iam_policy" "jenkins_s3_policy" {
+  name        = "jenkins-s3-access-policy"
+  description = "Allow EC2 instance to read and write to Jenkins S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = aws_s3_bucket.jenkins_artifacts.arn
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "${aws_s3_bucket.jenkins_artifacts.arn}/*"
+      }
+    ]
+  })
+}
+
+# IAM Role for EC2
+resource "aws_iam_role" "jenkins_role" {
+  name = "jenkins-ec2-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach Policy to Role
+resource "aws_iam_role_policy_attachment" "jenkins_s3_attach" {
+  role       = aws_iam_role.jenkins_role.name
+  policy_arn = aws_iam_policy.jenkins_s3_policy.arn
+}
+
+# Instance Profile for EC2
+resource "aws_iam_instance_profile" "jenkins_instance_profile" {
+  name = "jenkins-instance-profile"
+  role = aws_iam_role.jenkins_role.name
 }
